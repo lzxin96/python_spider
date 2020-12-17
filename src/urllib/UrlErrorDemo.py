89453b4216967208417278ac403d2b55ffa95aeb
lzxin96 <545634702@qq.com>
2020-12-17 23:35:45 +0800

    UrlError

--- src/urllib/UrlErrorDemo.py
+++ src/urllib/UrlErrorDemo.py
@@ -1,25 +1,25 @@
 from urllib import request,error
 
 try:
-    response =  request.urlopen('http://www.jd123.com/test.html')
+    response =  request.urlopen("http://www.jd123.com/test.html")
 except error.URLError as e:
     # Bad Request
     print(e.reason)
 
 try:
-    response = request.urlopen('https://geekori.com/abc.html')
+    response = request.urlopen("https://geekori.com/abc.html")
 except error.URLError as e:
     # Not Found
     print(e.reason)
 
 try:
-    response = request.urlopen('https://111geekori123222222.com/abc.html')
+    response = request.urlopen("https://geekori123.com/abc.html")
 except error.URLError as e:
     # [Error 8] nodename nor servname provided, or not known
     print(e.reason)
 
 try:
-    response = request.urlopen('https://baidu.com', timeout=0.0001)
+    response = request.urlopen("https://baidu.com", timeout=0.0001)
 except error.URLError as e:
     # Time out
     print(e.reason)
