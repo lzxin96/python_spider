4df16dabaaa5ea5fb7b74f0c036626e2b26da017
lzxin96 <545634702@qq.com>
2020-12-29 23:29:44 +0800

    http上传文件

---
+++ src/urllib3/upload_server.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, request
+
+# 定义服务端保存上传文件的位置
+UPLOAD_FOLDER = 'uploads'
+app = Flask(__name__)
+# 用于接收上传文件的路由需要使用POST方法
+@app.route('/', methods=['POST'])
+def upload_file():
+    # 获取上传文件的内容
+    file = request.files['file']
+    if file:
+        # 将上传的文件保存到uploads子目录中
+        file.save(os.path.join(UPLOAD_FOLDER, os.path.basename(file.filename)))
+        return "文件上传成功"
+
+if __name__ == '__main__':
+    app.run()
