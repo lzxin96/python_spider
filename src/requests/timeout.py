371c3eb5da8b53f6080f1781e67cf7ae14519486
lzxin96 <545634702@qq.com>
2021-01-10 22:54:54 +0800

    连接超时，读取超时

--- src/requests/timeout.py
+++ src/requests/timeout.py
@@ -2,13 +2,13 @@ import requests,requests.exceptions
 
 try:
     # 会抛出超时异常,连接和读取时间不能超过0.01秒
-    r = requests.get('http://www.jd.com', timeout=0.01)
+    r = requests.get('https://www.jd.com', timeout=0.01)
     print(r.text)
 except requests.exceptions.Timeout as e:
     print(e)
 
 # 抛出连接超时异常
-requests.get('http://www.jd.com', timeout=(0.001,0.01))
+requests.get('https://www.jd.com', timeout=(0.001,0.01))
 
 # 永久等待，不会抛出异常超时
-requests.get('http://www.jd.com', timeout=None)
+requests.get('https://www.jd.com', timeout=None)
