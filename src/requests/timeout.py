c83cbd12ce0c3ab74591be936389d722eadfe940
lzxin96 <545634702@qq.com>
2021-01-10 22:16:26 +0800

    超时连接处理

---
+++ src/requests/timeout.py
@@ -0,0 +1,14 @@
+import requests,requests.exceptions
+
+try:
+    # 会抛出超时异常,连接和读取时间不能超过0.01秒
+    r = requests.get('http://www.jd.com', timeout=0.01)
+    print(r.text)
+except requests.exceptions.Timeout as e:
+    print(e)
+
+# 抛出连接超时异常
+requests.get('http://www.jd.com', timeout=(0.001,0.01))
+
+# 永久等待，不会抛出异常超时
+requests.get('http://www.jd.com', timeout=None)
